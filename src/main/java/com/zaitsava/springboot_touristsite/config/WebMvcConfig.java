96f8a487da9ea7f3d5eb65aa12e895b6c77c7428
Yana Zaitsava <angels1070@gmail.com>
2022-04-12 19:22:16 +0300

    added icon and test image

--- src/main/java/com/zaitsava/springboot_touristsite/config/WebMvcConfig.java
+++ src/main/java/com/zaitsava/springboot_touristsite/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.zaitsava.springboot_touristsite.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,8 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return bCryptPasswordEncoder;
     }
 
+
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("user/login");
+
     }
 }
